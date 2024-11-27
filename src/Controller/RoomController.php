9c0a9260cf56d53dc749ee566d1af2dd75cf7450
Galoyer Marc <mgaloyer@uneak.fr>
2024-11-27 19:43:35 -0400

    room crud refactoring

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipment;
 use App\Entity\Room;
 use App\Form\RoomType;
 use App\Repository\RoomRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('admin/room', name: 'app_room_')]
 class RoomController extends AbstractController
 {
-    #[Route('/', name: 'list')]
+    #[Route('/', name: 'list', methods: ['GET'])]
     public function index(RoomRepository $repository): Response
     {
         return $this->render('room/list.html.twig', [
@@ -23,7 +23,7 @@ class RoomController extends AbstractController
         ]);
     }
 
-    #[Route('/create', name: 'create')]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function create(EntityManagerInterface $em, Request $request): Response
     {
         $room = new Room();
@@ -40,10 +40,19 @@ class RoomController extends AbstractController
 
         return $this->render('room/form.html.twig', [
             'form' => $form,
+            'room' => $room,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'edit')]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(RoomRepository $repository, int $id): Response
+    {
+        return $this->render('room/show.html.twig', [
+            'room' => $repository->find($id),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(EntityManagerInterface $em, Request $request, int $id): Response
     {
         $room = $em->getRepository(Room::class)->find($id);
@@ -63,4 +72,18 @@ class RoomController extends AbstractController
             'room' => $room,
         ]);
     }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(EntityManagerInterface $em, Request $request, int $id): Response
+    {
+        $room = $em->getRepository(Room::class)->find($id);
+
+        if ($this->isCsrfTokenValid('delete'.$room->getId(), $request->getPayload()->getString('_token'))) {
+            $em->remove($room);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('app_room_list', [], Response::HTTP_SEE_OTHER);
+    }
+
 }
