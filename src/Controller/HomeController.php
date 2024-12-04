2afc71fd0725666c1b9d0c8cd7f83b05a7bcd888
Galoyer Marc <mgaloyer@uneak.fr>
2024-12-04 11:20:39 -0400

    fix HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,23 @@
 
     namespace App\Controller;
 
+    use App\Core\Services\Manager\RoomManager;
     use App\Repository\RoomRepository;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\PropertyAccess\PropertyAccess;
     use Symfony\Component\Routing\Attribute\Route;
+    use Symfony\Component\Serializer\SerializerInterface;
 
     class HomeController extends AbstractController
     {
         #[Route('/', name: 'app_home')]
-        public function index(RoomRepository $repository): Response
+        public function index(RoomManager $manager, SerializerInterface $serializer): Response
         {
+
+
             return $this->render('front/home.html.twig', [
-                'rooms' => $repository->findAll(),
+                'rooms' => $manager->all(),
             ]);
         }
     }
