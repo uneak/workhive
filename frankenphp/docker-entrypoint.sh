0432e860262daee6c720bba72471976c0ba61bf4
Galoyer Marc <mgaloyer@uneak.fr>
2024-12-07 13:57:45 -0400

    docker error

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -28,14 +28,14 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 	if grep -q ^DATABASE_URL= .env; then
 		echo 'Waiting for database to be ready...'
-		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
+		ATTEMPTS_LEFT_TO_REACH_DATABASE=120
 		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
 			if [ $? -eq 255 ]; then
 				# If the Doctrine command exits with 255, an unrecoverable error occurred
 				ATTEMPTS_LEFT_TO_REACH_DATABASE=0
 				break
 			fi
-			sleep 1
+			sleep 2
 			ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
 			echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
 		done
@@ -55,6 +55,11 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+
+	# Generate JWT keys if they don't exist
+	if [ ! -f ./config/jwt/private.pem ] || [ ! -f ./config/jwt/public.pem ]; then
+		php bin/console lexik:jwt:generate-keypair --skip-if-exists
+	fi
 fi
 
 exec docker-php-entrypoint "$@"
